a519fd932dcf929b831e56b658f326c3e3f70a77
larrygdyck <95650324+larrygdyck@users.noreply.github.com>
2025-08-30 08:56:22 -0600

    Added variable line width
    Replaced ThreeJS Line with Line2 to support variable line width for rods and tracer.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { Line2 } from 'three/addons/lines/Line2.js';
+import { LineGeometry } from 'three/addons/lines/LineGeometry.js';
+import { LineMaterial } from 'three/addons/lines/LineMaterial.js';
 import { x1,y1,x2,y2,positions1,positions2,updatePendulum,speedFactor,limit,lastIndex,i } from './pendulum.js';
 let j = 0;
 //let traceArray = [new THREE.Vector3(0,0,0)]; 
@@ -31,16 +34,17 @@ pendulumBob1.position.set(x1, y1, 0);
 const pendulumBob2 = new THREE.Mesh(geometry, material2);
 pendulumBob2.position.set(x2, y2, 0);
 
-const rodMaterial = new THREE.LineBasicMaterial({ color: 'white' });
-const traceMaterial = new THREE.LineBasicMaterial({ color: 'green' });
-//const traceMaterial2 = new THREE.LineBasicMaterial({ color: 'orange' });    
-const rodGeometry1 = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0, 0, 0), new THREE.Vector3(x1, y1, 0)]);
-const rodGeometry2 = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(x1, y1, 0), new THREE.Vector3(x2, y2, 0)]);
-const traceGeometry = new THREE.BufferGeometry().setFromPoints(positions2);
 
-const rod1 = new THREE.Line(rodGeometry1, rodMaterial);
-const rod2 = new THREE.Line(rodGeometry2, rodMaterial);
-const lineTrace = new THREE.Line(traceGeometry, traceMaterial);
+const rodGeometry1 = new LineGeometry().setFromPoints([new THREE.Vector3(0, 0, 0), new THREE.Vector3(x1, y1, 0)]);
+const rodGeometry2 = new LineGeometry().setFromPoints([new THREE.Vector3(x1, y1, 0), new THREE.Vector3(x2, y2, 0)]);
+const traceGeometry = new LineGeometry().setFromPoints(positions2);
+
+const rodMaterial = new LineMaterial({ linewidth: 1, color: 0xffffff });
+const traceMaterial = new LineMaterial({ linewidth: 1, color: 0x008000 }); 
+
+const rod1 = new Line2(rodGeometry1, rodMaterial);
+const rod2 = new Line2(rodGeometry2, rodMaterial);
+const lineTrace = new Line2(traceGeometry, traceMaterial);
 
 scene.add(lineTrace);
 scene.add(pendulumBob1);
